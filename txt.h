c9025dcb4427be7e44953fcc473222b26c08b4f1
hdj <946973965@qq.com>
2016-04-08 22:23:05 +0800

    txt

---
+++ txt.h
@@ -0,0 +1,15 @@
+#ifndef txt_h
+#define txt_h
+
+typedef struct result{
+	int line;
+	char **parsing_result;
+}result;
+
+typedef struct message{
+	char *buf;
+}message;
+struct result* parsing_txt(struct message* head);
+
+
+#define
