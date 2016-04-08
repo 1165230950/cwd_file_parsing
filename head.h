68a1300f93057691c47293c2802be6e155924193
hdj <946973965@qq.com>
2016-04-08 23:29:56 +0800

    main

--- head.h
+++ head.h
@@ -2,6 +2,7 @@
 #define head_h
 
 #define check_input if(argc >2) error()
+#define format char **fformat = {".xml", ".html"}
 void error();
 
 
--- main.c
+++ main.c
@@ -5,6 +5,17 @@
 
 int main(int argc, char *argv[])
 {
+	/*========================检查输入=========================*/
 	check_input;
+
+	/*======================判断文件格式=======================*/
+	int len = strlen(argv[1]);
+	if(!strcmp(argv[1]+len-4, format[0]))
+	{
+		message *p = malloc(message);
+		result *q = parsing_xml(p);
+	}
+
+
 	return 0;
 }
