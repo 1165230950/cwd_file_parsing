db15bc6330a67327ac53d3684f927e1d4dd1df59
hdj <946973965@qq.com>
2016-04-09 16:15:01 +0800

    remove *

--- head.c
+++
@@ -1,7 +0,0 @@
-#include "head.h"
-
-void error()
-{
-	printf("请输入正确格式：parsing+filename\n");
-	exit(0);
-}
--- head.h
+++
@@ -1,9 +0,0 @@
-#ifndef head_h
-#define head_h
-
-#define check_input if(argc >2) error()
-#define format char **fformat = {".xml", ".html"}
-void error();
-
-
-#endif
